44d75ec441fe98dba6ecbfa17507c5968c27971d
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-16 11:43:00 +0100

    First commit

---
+++ src/test/java/samuelesimeone/GestioneDispositiviAziendali/GestioneDispositiviAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelesimeone.GestioneDispositiviAziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDispositiviAziendaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
